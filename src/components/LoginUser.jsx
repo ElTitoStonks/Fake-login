02f3daf3a278f874b34af24700358556cddd53a9
ElTitoStonks <marioodevv@gmail.com>
2025-05-23 08:46:25 +0200

    style: adding styles form for mobile device(not finish)

--- src/components/LoginUser.jsx
+++ src/components/LoginUser.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { FilterContext } from "../context/AuthProvider";
+import { FormLogicRegister } from "../hooks/FormLogic";
+import { EmailIcon, PasswordIcon } from "../../public/Icons"
 
 export function LoginUser({ handleLogout }) {
     const { user } = useContext(FilterContext)
@@ -13,29 +15,66 @@ export function LoginUser({ handleLogout }) {
 
 export function NoLoginUser({ handleSubmit, handleChange, formData, error }) {
     return (
-        <div>
-            <h2>Hey! to see all information login in the platform</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="email" placeholder="Email" name='email' onChange={handleChange} value={formData.email} />
-                <input type="password" placeholder="Password" name='password' onChange={handleChange} value={formData.password} />
+        <div className="h-[80dvh] w-[90dvw] bg-[#FFFFFF] py-6">
+            <div className="flex flex-col justify-center items-center mb-6">
+                <h2 className="text-black font-bold text-4xl text-center">Welcome back</h2>
+                <p className="text-gray-600">Please sing in to your account</p>
+            </div>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 px-6">
+                <div className=" flex flex-col w-full">
+                    <label className="text-black" id="email">Email address</label>
+                    <div className="relative flex items-center">
+                        <span className="absolute px-2"><EmailIcon /></span>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="example@example.com"
+                            name='email'
+                            onChange={handleChange}
+                            value={formData.email}
+                            className="text-gray-600 border border-gray-300 w-full h-10 px-2 pl-7 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <label id="password">Password</label>
+                    <div className="relative flex items-center">
+                        <span className="absolute px-2"><PasswordIcon /></span>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name='password'
+                            onChange={handleChange}
+                            value={formData.password}
+                            className="text-gray-600 border border-gray-300 w-full h-10 px-2 pl-7 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <input type="checkbox" id="remember" />
+                        <label htmlFor="remember">Remember me</label>
+                    </div>
+
+                    <button>
+                        <p>Forgot your password?</p>
+                    </button>
+
+                </div>
                 <input type="submit" value="Login" />
             </form>
-            <ErrorLogin error={error} />
+            {error && <ErrorLogin error={error} />}
         </div>
     )
 }
 
 function ErrorLogin({ error }) {
+    const { errorR, formDataR, handleChangeR, handleSubmitR } = FormLogicRegister()
+
     return (
         <div>
             <p>{error}</p>
-        </div>
-    )
-}
-
-export function RegisterUser({ handleSubmitR, handleChangeR, formDataR, errorR }) {
-    return (
-        <div>
             <h2>You're new here?</h2>
             <form onSubmit={handleSubmitR}>
                 <input type="email" name="email" placeholder="Email" onChange={handleChangeR} value={formDataR.email} />
@@ -43,15 +82,8 @@ export function RegisterUser({ handleSubmitR, handleChangeR, formDataR, errorR }
                 <input type="text" name="name" placeholder="Name" onChange={handleChangeR} value={formDataR.name} />
                 <input type="submit" value="Register" />
             </form>
-            <ErrorRegister errorR={errorR} />
-        </div>
-    )
-}
+            {errorR && <p style={{ color: 'red' }}>{errorR}</p>}
 
-function ErrorRegister({ errorR }) {
-    return (
-        <div>
-            <p>{errorR}</p>
         </div>
     )
 }
